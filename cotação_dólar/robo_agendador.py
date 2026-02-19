e3dc85fa8257926f5ad83c0b5adb5242bd4fe73c
Luis Vasconcelos <luisfellipesilva@outlook.com>
2026-02-19 10:25:51 -0300

    ajuste no tempo

--- cotação_dólar/robo_agendador.py
+++ cotação_dólar/robo_agendador.py
@@ -84,8 +84,8 @@ def tirar_print():
             ChromeDriverManager().install()), options=chrome_options)
         driver.get(URL_DASHBOARD)
 
-        print("⏳ Aguardando renderização (50s)...")
-        time.sleep(50)
+        print("⏳ Aguardando renderização (60s)...")
+        time.sleep(60)
 
         driver.save_screenshot(caminho_completo)
         print(f"✅ Print salvo em: {caminho_completo}")
