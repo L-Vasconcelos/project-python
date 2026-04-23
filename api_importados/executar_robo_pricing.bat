ed8ce51be14b40063610c616a60d1b7155807b68
Luis Vasconcelos <luisfellipesilva@outlook.com>
2026-04-23 15:04:52 -0300

    ajuste venv

--- api_importados/executar_robo_pricing.bat
+++ api_importados/executar_robo_pricing.bat
@@ -2,3 +2,4 @@
 cd /d "C:\Users\lsilva\OneDrive\Arquivos\Python\api_importados"
 "C:\Users\lsilva\OneDrive\Arquivos\Python\.venv\Scripts\python.exe" "C:\Users\lsilva\OneDrive\Arquivos\Python\api_importados\robo_pricing.py" >> "C:\Users\lsilva\OneDrive\Arquivos\Python\api_importados\log_bat.txt" 2>&1
 echo Codigo de saida: %ERRORLEVEL% >> "C:\Users\lsilva\OneDrive\Arquivos\Python\api_importados\log_bat.txt"
+exit /b %ERRORLEVEL%
